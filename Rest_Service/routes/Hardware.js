19e3b0346dd40076c4cbe8e3caec53deebb43333
rene1997 <renekeijzer1997@gmail.com>
2017-04-05 14:44:58 +0200

    database sync added

--- Rest_Service/routes/Hardware.js
+++ Rest_Service/routes/Hardware.js
@@ -2,6 +2,7 @@
  * Created by rene on 4/5/17.
  */
 
+var moment = require('moment');
 var mongoose = require('mongoose');
 var mongoDatabase = "mongodb://localhost/SmartMeter";
 
@@ -36,8 +37,50 @@ function connectToDatabase(tablename, tableSchema, callback){
     db.once('open', function(){callback(table)});
 }
 
+function Callback(err, data){
+    if(err) return err;
+    return data;
+}
+
+function SyncMeasurements(hardwareDevices) {
+    connectToDatabase(LiveMeasurements, LiveMeasurementsSchema, function (table) {
+        for(var i = 0; i < hardwareDevices.length; i ++){
+            var deviceId = hardwareDevices[i].hardwareId;
+            table.find({hardwareId: deviceId}, function (err, data) {
+                if(err) console.log(err);
+                else addLiveDataToHistory(data);
+            });
+        }
+    });
+}
+
+function addLiveDataToHistory(liveData){
+    var hardwareId = liveData[0].hardwareId;
+    var totalKwh = 0;
+    var date = moment().add(2,'hours');
+    date = date.add(-1, 'hours');
+
+    for(var i = 0; i < liveData.length; i ++){
+        if(liveData[i].kwh || liveData[i].kwh != null)
+            totalKwh += liveData[i].kwh;
+    }
+    console.log("device: " + hardwareId);
+    console.log("total kwh: " + totalKwh);
+    console.log("date: " + date.format());
+    connectToDatabase(HistoryMeasurement, HistoryMeasurementSchema, function (table) {
+        var newMeasurement = new table({hardwareId: hardwareId, usedPower: totalKwh, Time: date.format()});
+        newMeasurement.save(function(err){
+            if(err) {
+                console.log(err);
+            }else{
+                console.log("added: " + newMeasurement);
+            }
+        });
+    });
+}
+
 module.exports = {
-   AddMeasurement : function(req, res){
+    AddMeasurement : function(req, res){
         //read body data
         var hardwareId = req.body.hardwareId;
         var kWh = req.body.kwh;
@@ -64,9 +107,9 @@ module.exports = {
                 }
             });
         });
-   },
+    },
 
-   GetCurrentMeasurement : function(req,res){
+    GetCurrentMeasurement : function(req,res){
        var hardwareId = req.body.hardwareId;
        connectToDatabase(LiveMeasurements, LiveMeasurementsSchema, function(table){
            table.findOne({hardwareId : hardwareId},{},{sort:{Time:-1}},function (err, data) {
@@ -78,9 +121,19 @@ module.exports = {
                res.json(data);
            });
        });
-   },
+    },
 
     SyncMeasurementData : function (req, res) {
-        //todo
+
+        //get all hardware id's:
+        connectToDatabase(HardwareDevices,HardwareDevicesSchema, function (table) {
+            table.find({}, function (err, data) {
+                if(err)
+                    return console.log(err);
+                SyncMeasurements(data);
+            });
+        });
     }
 }
+
+
