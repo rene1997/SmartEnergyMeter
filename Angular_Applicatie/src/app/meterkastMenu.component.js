7bdfa2b9fbeefb7c9ac0d28b4b8ea14043cc0698
rene1997 <renekeijzer1997@gmail.com>
2017-04-11 19:23:52 +0200

    live data works :)

--- Angular_Applicatie/src/app/meterkastMenu.component.js
+++ Angular_Applicatie/src/app/meterkastMenu.component.js
@@ -9,10 +9,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var http_1 = require('@angular/http');
+var http_2 = require('@angular/http');
 var MeterkastMenuComponent = (function () {
-    function MeterkastMenuComponent() {
-        this.gettodoUrls = "http://keijzersoft.nl:8081/apiV1/gettodos";
-        this.addTodoUrl = "http://keijzersoft.nl:8081/apiV1/addtodo";
+    function MeterkastMenuComponent(route, router, http) {
+        this.route = route;
+        this.router = router;
+        this.http = http;
+        this.liveMeasurementUrl = "http://keijzersoft.nl:8081/apiV1/getCurrentSpeed";
         this.lineChartData = [
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
@@ -26,6 +31,10 @@ var MeterkastMenuComponent = (function () {
     }
     MeterkastMenuComponent.prototype.ngOnInit = function () {
         var data = document.getElementById("hardwareData");
+        if (!data)
+            return this.router.navigate(['login']);
+        console.log(data.innerHTML);
+        this.refreshPowerUsage(data.innerHTML);
     };
     MeterkastMenuComponent.prototype.randomizeType = function () {
         this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
@@ -37,6 +46,34 @@ var MeterkastMenuComponent = (function () {
     MeterkastMenuComponent.prototype.chartHovered = function (e) {
         console.log(e);
     };
+    MeterkastMenuComponent.prototype.refreshPowerUsage = function (hardwareData) {
+        var hardwareId = JSON.parse(hardwareData).hardwareId;
+        this.getLiveMeasurements(hardwareId);
+        var self = this;
+        setInterval(function () {
+            self.getLiveMeasurements(hardwareId);
+        }, 10000);
+    };
+    MeterkastMenuComponent.prototype.getLiveMeasurements = function (hardwareId) {
+        var _this = this;
+        var headers = new http_2.Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        var options = new http_2.RequestOptions({ headers: headers });
+        var urlSearchParams = new http_2.URLSearchParams();
+        urlSearchParams.append('hardwareId', '' + hardwareId);
+        urlSearchParams.append('serverKey', '175d6c2c2632e0f87a07f32e88a690104f921b517c7af1c6333de2dfad9be8e3');
+        var body = urlSearchParams.toString();
+        return this.http.post(this.liveMeasurementUrl, body, options).subscribe(function (data) { return _this.setLiveMeasurement(data); }, function (err) { return _this.handleError(err); }, function () { return console.log("Hardware request complete"); });
+    };
+    MeterkastMenuComponent.prototype.setLiveMeasurement = function (data) {
+        var obJson = JSON.parse(data['_body']);
+        console.log(obJson);
+        var speed = document.getElementById("currentSpeed");
+        var speedNumber = "" + obJson.wh;
+        speed.innerHTML = speedNumber.split('.')[0];
+    };
+    MeterkastMenuComponent.prototype.handleError = function (error) {
+        console.info(error.toString());
+    };
     MeterkastMenuComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
@@ -44,7 +81,7 @@ var MeterkastMenuComponent = (function () {
             templateUrl: './meterkastMenu.component.html',
             styleUrls: ['./meterkastMenu.component.css']
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [router_1.ActivatedRoute, router_1.Router, http_1.Http])
     ], MeterkastMenuComponent);
     return MeterkastMenuComponent;
 }());
--- Angular_Applicatie/src/app/meterkastMenu.component.ts
+++ Angular_Applicatie/src/app/meterkastMenu.component.ts
@@ -11,8 +11,7 @@ import { Headers, RequestOptions, URLSearchParams } from '@angular/http';
 })
 
 export class MeterkastMenuComponent{
-  private gettodoUrls= "http://keijzersoft.nl:8081/apiV1/gettodos";
-  private addTodoUrl= "http://keijzersoft.nl:8081/apiV1/addtodo";
+    private liveMeasurementUrl = "http://keijzersoft.nl:8081/apiV1/getCurrentSpeed";
 
   public lineChartData:Array<any> = [
     [65, 59, 80, 81, 56, 55, 40],
@@ -26,11 +25,17 @@ export class MeterkastMenuComponent{
   public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
   public pieChartData:number[] = [300, 500, 100];
 
+    constructor(
+        private route:ActivatedRoute,
+        private router:Router,
+        private http:Http,
+    ){}
+
   ngOnInit() {
     var data = document.getElementById("hardwareData");
-
-
-
+    if(!data) return this.router.navigate(['login']);
+    console.log(data.innerHTML);
+    this.refreshPowerUsage(data.innerHTML);
   }
 
   public randomizeType():void {
@@ -45,4 +50,41 @@ export class MeterkastMenuComponent{
   public chartHovered(e:any):void {
     console.log(e);
   }
+
+  private refreshPowerUsage(hardwareData: string){
+      var hardwareId = JSON.parse(hardwareData).hardwareId;
+      this.getLiveMeasurements(hardwareId);
+      var self = this;
+      setInterval(function () {
+            self.getLiveMeasurements(hardwareId);
+      },10000);
+  }
+
+private getLiveMeasurements(hardwareId : Number){
+    let headers = new Headers({'Content-Type':'application/x-www-form-urlencoded'});
+    let options = new RequestOptions({headers:headers});
+    let urlSearchParams = new URLSearchParams();
+
+    urlSearchParams.append('hardwareId', '' + hardwareId);
+    urlSearchParams.append('serverKey', '175d6c2c2632e0f87a07f32e88a690104f921b517c7af1c6333de2dfad9be8e3');
+    let body = urlSearchParams.toString();
+    return this.http.post(this.liveMeasurementUrl , body, options).subscribe(
+        data  => this.setLiveMeasurement(data),
+        err   => this.handleError(err),
+        ()    => console.log("Hardware request complete")
+    )
+}
+
+    setLiveMeasurement(data:Response){
+        var obJson = JSON.parse(data['_body']);
+        console.log(obJson);
+
+        let speed = document.getElementById("currentSpeed");
+        let speedNumber = "" + obJson.wh;
+        speed.innerHTML = speedNumber.split('.')[0];
+    }
+
+    handleError(error:Response){
+        console.info(error.toString());
+    }
 }
